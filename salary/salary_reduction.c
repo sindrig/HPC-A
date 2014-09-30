32fa667836c5fd067b5cc6237ec20c06799baff8
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2014-09-30 16:21:56 +0000

    Added region where we specify the number of threads in a parallel region

--- salary/salary_reduction.c
+++ salary/salary_reduction.c
@@ -5,7 +5,7 @@
 
 int main (int argc, char *argv[])
 {
-        int i, nthreads, tid,  programmers, managers, accounters, ceos;
+        int i, nthreads, tid,  programmers, managers, accounters, ceos, num_workers;
         float accounterFee, programmerSalary, managerSalary, ceoSalary, totalSalary;
 
         /* Some initializations */
@@ -31,29 +31,33 @@ int main (int argc, char *argv[])
         }
 
         #pragma omp parallel for reduction(+:totalSalary)
-        for(i = 0; i < accounters; i++){
-            printf("\nAccounter %d calculating in loop 1.\n", omp_get_thread_num());
-            sleep(omp_get_thread_num());
-            totalSalary = totalSalary + accounterFee;
-            }
-        #pragma omp parallel for reduction(+:totalSalary)
         for(i = 0; i < programmers; i++){
             printf("\nAccounter %d calculating in loop 2.\n", omp_get_thread_num());
-            sleep(omp_get_thread_num());
             totalSalary = totalSalary + programmerSalary;
         }
         #pragma omp parallel for reduction(+:totalSalary)
         for(i = 0; i < managers ; i++){
             printf("\nAccounter %d calculating in loop 3.\n", omp_get_thread_num());
-            sleep(omp_get_thread_num());
             totalSalary = totalSalary + managerSalary;
         }
         #pragma omp parallel for reduction(+:totalSalary)
         for(i = 0; i < ceos; i++){
             printf("\nAccounter %d calculating in loop 4.\n", omp_get_thread_num());
-            sleep(omp_get_thread_num());
             totalSalary = totalSalary + ceoSalary;
         }
+
+        num_workers = programmers + managers + ceos;
+        #pragma omp parallel for num_threads(num_workers) private(tid)
+        for(i = 0; i < num_workers; i++){
+            tid = omp_get_thread_num();
+            printf("I'm employee #%d and I do exist\n", tid);
+        }
+        
+        #pragma omp parallel for reduction(+:totalSalary)
+        for(i = 0; i < accounters; i++){
+            printf("\nAccounter %d calculating in loop 1.\n", omp_get_thread_num());
+            totalSalary = totalSalary + accounterFee;
+            }
 
         printf("\nA company has to pay $%f total salary for the current month. The company consists of %d programmers ($%f a month), %d managers ($%f a month), %d ceos ($%f a month) and additionally, %d accounters were hired to calculate the total salary and they charged $%f each for their work.\n\n", totalSalary, programmers, programmerSalary, managers, managerSalary, ceos, ceoSalary, accounters, accounterFee);
 }
