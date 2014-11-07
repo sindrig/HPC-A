cdd053c1ba66473693ab656fcf02b03fa553059d
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2014-11-07 11:45:16 +0000

    Visualizing

--- fish/tests/visualize/visualize.py
+++ fish/tests/visualize/visualize.py
@@ -3,17 +3,25 @@
 import pickle
 import sys
 
-with open('first.c', 'r') as f:
-    for line in f:
-        if 'POPULATION' in line and 'define' in line:
-            nfish = int(line.strip().split()[-1])
-            break
 
-fish_received = []
+def print_fish(data):
+    for item in data:
+        print item
 
 
-def print_fish(fishes):
-    pass
+def get_file_scp(username, password):
+    # Assumes that the datafile resides at
+    # ~/HPC-A/fish/tests/visualize/output.dat
+    import paramiko
+    import scp
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect('jotunn.rhi.hi.is', 22, username, password)
+    scp_client = scp.SCPClient(client.get_transport())
+    scp_client.get('~/HPC-A/fish/tests/visualize/output.dat')
+    with open('output.dat', 'r') as f:
+        return pickle.load(f)
 
 
 class Fish(object):
@@ -23,7 +31,31 @@ class Fish(object):
         self.x = x
         self.y = y
 
+    def __unicode__(self):
+        return '(%d, %d): %d' % (self.x, self.y, self.num)
+
+
+class Parameter(object):
+    value = 0
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_defined_parameter(self, f):
+        for line in f:
+            if self.name in line and 'define' in line:
+                self.value = int(line.strip().split()[-1])
+
+    def __unicode__(self):
+        return '%s = %d' % (self.name, self.value)
+
+
 if sys.argv[-1] == 'save':
+    data = []
+    with open('../first.c', 'r') as f:
+        for p in ('POPULATION', 'WORLD_HEIGHT', 'WORLD_WIDTH'):
+            param = Parameter(p)
+            data.append(p.get_defined_parameter(f))
     while 1:
         try:
             line = sys.stdin.readline()
@@ -37,9 +69,11 @@ if sys.argv[-1] == 'save':
             parttype = parts[0]
             if parttype == 'fish':
                 iteration, num, x, y = (int(k) for k in parts[1:])
-                fish_received.append(Fish(iteration, num, x, y))
+                data.append(Fish(iteration, num, x, y))
     with open('output.dat', 'w') as f:
-        pickle.dump(fish_received, f)
-elif len(sys.argv) > 2 and sys.argv[1] == 'gif':
+        pickle.dump(data, f)
+elif len(sys.argv) == 2 and sys.argv[1] == 'gif':
     with open(sys.argv[-1], 'r') as f:
         print_fish(pickle.load(f))
+elif len(sys.argv) == 4 and sys.argv[1] == 'gif':
+    print_fish(get_file_scp(*sys.argv[2:]))
